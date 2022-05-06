8b71384d523288588353d0d9ff51b33b724bea7a
Arghavan Dor <97262547+ArghavanDor18@users.noreply.github.com>
2022-05-06 02:09:42 -0700

    Assignment_1
    Added index.html, search.html, code.js, search.js, server.js
    
    Displayed 9 random Pokemons in a 3x3 flexbox. Designed footer, navbar, and header.

---
+++ Assignment_1/public/code.js
@@ -0,0 +1,43 @@
+to_add = ''
+
+function processPokeResp(data){
+    // 3 - process the response and extract the img
+    to_add += `${data.name} 
+    <div class="image_container"> 
+    <a href="/profile/${data.id}"> 
+    <img src="${data.sprites.other["official-artwork"].front_default}">
+    </a>
+    </div>`
+}
+
+async function loadNineImages() {
+    to_add += `<div class="flex-container">`
+    for (i = 1; i <= 9; i++) { // Nine times
+        //if (i % 3 == 1) { // only when i= 1, 4, 7
+            to_add += `<div class="images_group">`
+        //}
+        // 1 - generate random numbers
+        x = Math.floor(Math.random() * 100) + 1
+        // 2 - init a AJAX request to pokeapi.co
+        await $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${x}/`,
+            success: processPokeResp
+        })
+        //to_add += ` <div class="image_container"> <img src="${data.sprites.other["official-artwork"].front_default}> </div>`
+
+        //if (i % 3 == 0) { // only when i= 3, 6, 9
+        
+            to_add += `</div>`
+        //}
+    }
+    to_add += `</div>`
+    jQuery("main").append(to_add)
+}
+
+function setup() {
+    loadNineImages();
+    // events handlers
+}
+
+jQuery(document).ready(setup)
